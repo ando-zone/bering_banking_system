73aa13a73cd4f6c20cc968837539c8eaa12177f7
ando-zone <dev.dohyeonan@gmail.com>
2023-05-21 00:17:41 +0900

    chore: 불필요한 주석 삭제

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -4,5 +4,4 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def home():
-    #return render_template('index.html', question_num=1)
     return jsonify(message="Welcome to Bering Bank!")
