3195734dd1aebe4d4bed9dcef1231741cf227fa0
ando-zone <dev.dohyeonan@gmail.com>
2023-05-21 16:07:40 +0900

    feat: index(or home) page 수정

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, current_app
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def home():
-    return jsonify(message="Welcome to Bering Bank!")
+    urls = []
+    for rule in current_app.url_map.iter_rules():
+        if rule.endpoint != 'static':
+            url = {
+                'url': rule.rule,
+                'endpoint': rule.endpoint
+            }
+            urls.append(url)
+
+    return jsonify(message="Welcome to Bering Bank!", urls=urls)
+
