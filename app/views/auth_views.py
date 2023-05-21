73e9ca03d288e4588b3d2ee65f09a05aa85ab572
ando-zone <dev.dohyeonan@gmail.com>
2023-05-21 15:15:56 +0900

    feat: auth_views.py에 logging 설정 및 MeView 클래스에 put, delete 메서드 추가

--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -1,6 +1,6 @@
 import functools
 
-from flask import Blueprint, request, session, jsonify, g, redirect, url_for
+from flask import Blueprint, request, session, jsonify, g, redirect, url_for, current_app
 from flask.views import MethodView
 
 from app.models import User
@@ -17,12 +17,14 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = User.query.get(user_id)
+        current_app.logger.info(f"Loaded user with id {user_id}")
 
 
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
+            current_app.logger.warning("Unauthorized access attempt")
             return redirect(url_for("auth.login"))
 
         return view(**kwargs)
@@ -37,7 +39,49 @@ class MeView(MethodView):
         user_id = g.user.id
         user = User.query.get(user_id)
 
+        current_app.logger.info(f"Fetched user info for id {user_id}")
+
         return jsonify(user.to_dict()), 200
+
+    def put(self):
+        user_id = g.user.id
+        user = User.query.get(user_id)
+
+        username = request.json.get('name')
+        current_password = request.json.get('current_password')
+        new_password = request.json.get('new_password')
+
+        error = None
+
+        if not current_password:
+            error = "Current password is required."
+        elif not user.verify_password(current_password):
+            error = "Current password is incorrect."
+
+        if username:
+            user.name = username
+
+        if new_password and error is None:
+            user.password = new_password
+        elif error is not None:
+            return jsonify({"error": error}), 400
+
+        db.session.commit()
+        current_app.logger.info(f"Updated username for user id {user_id}")
+        current_app.logger.info(f"Updated password for user id {user_id}")
+
+        return jsonify(user.to_dict()), 200
+
+    def delete(self):
+        user_id = g.user.id
+        user = User.query.get(user_id)
+
+        db.session.delete(user)
+        db.session.commit()
+
+        current_app.logger.info(f"Deleted user account for user id {user_id}")
+
+        return jsonify({"message": "Account deleted successfully"}), 200
 
 
 class UserCreateView(MethodView):
@@ -60,7 +104,11 @@ class UserCreateView(MethodView):
             new_user = User(name=username, email=email, password=password)
             db.session.add(new_user)
             db.session.commit()
+            current_app.logger.info(f"Created new user with email {email}")
+
             return jsonify({"message": "Account created successfully"})
+
+        current_app.logger.error(f"Account creation failed: {error}")
 
         return jsonify({"error": error}), 400
 
@@ -68,7 +116,10 @@ class UserCreateView(MethodView):
 class LogInView(MethodView):
     def get(self):
         if g.user:
+            current_app.logger.info(f"User already logged in.")
             return jsonify({"message": "You are already logged in!"}), 200
+
+        current_app.logger.info(f"Prompting user to log in.")
 
         return jsonify({"message": "Please Log into Bering Bank!"}), 200
 
@@ -87,7 +138,11 @@ class LogInView(MethodView):
         if error is None:
             session.clear()
             session["user_id"] = user.id
+            current_app.logger.info(f"User {username} logged in successfully.")
+
             return jsonify({"message": "Logged in successfully"})
+
+        current_app.logger.error(f"Login attempt failed for user {username}: {error}")
 
         return jsonify({"error": error}), 400
 
@@ -97,6 +152,8 @@ class LogOutView(MethodView):
 
     def post(self):
         session.clear()
+        current_app.logger.info(f"User logged out successfully.")
+
         return jsonify({"message": "Logged out successfully"})
 
 
