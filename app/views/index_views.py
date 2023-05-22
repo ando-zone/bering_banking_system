2c231d6b7052df33a02de51afadaa97236e91a43
ando-zone <dev.dohyeonan@gmail.com>
2023-05-22 21:16:35 +0900

    chore: index_views.py에 닥스트링 추가

--- app/views/index_views.py
+++ app/views/index_views.py
@@ -4,6 +4,27 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def home():
+    """Bering Bank의 홈 페이지를 렌더링함.
+
+    Returns:
+        환영 메시지와 URL 목록을 포함하는 JSON 응답.
+
+    Example:
+        {
+            "message": "Bering Bank에 오신 것을 환영합니다!",
+            "urls": [
+                {
+                    "url": "/",
+                    "endpoint": "home"
+                },
+                {
+                    "url": "/accounts",
+                    "endpoint": "accounts"
+                },
+                ...
+            ]
+        }
+    """
     urls = []
     for rule in current_app.url_map.iter_rules():
         if rule.endpoint != 'static':
